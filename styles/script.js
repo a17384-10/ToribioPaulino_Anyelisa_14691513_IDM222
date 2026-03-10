52ff8ecb66dc492b6b2614c64bbf6bf87b985548
a17384-10 <at3647@drexel.edu>
2026-03-10 14:18:02 -0400

    added night owls

--- styles/script.js
+++ styles/script.js
@@ -61,3 +61,19 @@ document.querySelectorAll('.grid-container img').forEach(image => {
   };
 });
 
+document.querySelectorAll('#slide-grid img').forEach(image => {
+  image.onclick = () => {
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('full-screen');
+
+    const bigImg = document.createElement('img');
+    bigImg.src = image.src;
+
+    overlay.appendChild(bigImg);
+    document.body.appendChild(overlay);
+
+    overlay.onclick = () => overlay.remove();
+  };
+});
+

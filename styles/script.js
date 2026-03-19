c3dd2f29ae53a8619ba47238ca66825f0a0b5774
a17384-10 <at3647@drexel.edu>
2026-03-18 21:31:40 -0400

    fixed dark mode

--- styles/script.js
+++ styles/script.js
@@ -1,3 +1,29 @@
+//dark mode
+window.addEventListener("DOMContentLoaded", function(){
+
+  var toggleBtn = document.querySelector(".theme-toggle");
+
+  // restore theme
+  var savedTheme = localStorage.getItem("theme");
+  if(savedTheme === "dark"){
+    document.body.classList.add("dark-mode");
+  }
+
+  toggleBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("dark-mode");
+    toggleBtn.textContent =
+  document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+
+    if(document.body.classList.contains("dark-mode")){
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+
+  });
+
+});
 
 //code for zooming into the pictures
 document.querySelectorAll('.grid-container img').forEach(image => {
